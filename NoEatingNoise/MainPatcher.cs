b3e233f9849a85abea2fa43aab09f83f629e5b13
Albin Eklundh <eklundh.albin@gmail.com>
2020-07-14 09:53:24 +0200

    add first version

---
+++ NoEatingNoise/MainPatcher.cs
@@ -0,0 +1,14 @@
+﻿using Harmony;
+using System.Reflection;
+
+namespace NoEatingNoise
+{
+    public class MainPatcher
+    {
+        public static void Patch()
+        {
+            var harmony = HarmonyInstance.Create("com.aeklundh.subnauticabelowzero.noeatingnoise.mod");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+}
